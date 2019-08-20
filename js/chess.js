ac285007bd1abb0eb8f9d370f90f224bf10f87af
marianawerneck <mwerneckroque00@gmail.com>
2019-08-20 13:01:25 -0300

    white simple piece verified move

--- js/chess.js
+++ js/chess.js
@@ -3,7 +3,7 @@ $(function(){
 	board = [[0,3,0,3,0,3,0,3],
                           [3,0,3,0,3,0,3,0],
                           [0,3,0,3,0,3,0,3],
-                          [1,0,1,0,1,0,1,0],
+                          [1,0,1,0,2,0,1,0],
                           [0,1,0,1,0,1,0,1],
                           [2,0,2,0,2,0,2,0],
                           [0,2,0,2,0,2,0,2],
@@ -27,15 +27,92 @@ $(function(){
 			}
 		}
 	}
-	/*
-	$('body').click(function(){
-		if (selected == 2){
-			$('#'+selectedPiece).fadeTo(10,1);
-			selectedPiece = '';
-			selected = 0;
-		}
-	});*/
 	
+	function makeMove(xi,yi,xf,yf,player){
+		
+		enemy = 0;
+        if (player == 2){
+            enemy = 3;
+		}
+        else if(player == 3){
+            enemy = 2;
+		}
+        else{
+            console.log('player nao existe');
+            return [];
+		}
+		
+		if (player == 3){
+			if(board[xi][yi] == 3){
+				var tBoard = moveUpDown(xi,yi,xf,yf,player,enemy);
+				return tBoard;
+			}else if(board[xi][yi] == -3){
+				//moveUpDown();
+				//moveDownUp();
+			}
+		}
+	}
+	
+	function moveUpDown(xi,yi,xf,yf,player,enemy){
+		/*check move direction*/
+		if( xf == (xi+1) && ((yf == yi -1) || (yf == yi + 1))){
+			valorPeca = board[xi][yi];
+			/*check enemy existence*/
+			if(board[xf][yf] == enemy || board[xf][yf] == enemy*(-1)){
+				if ((xi+2) < 8){
+					/*verifica a direcao do movimento*/
+                    if (yf == yi -1){
+						if (yf -1 >= 0){
+							if (board[xf+1][yf-1] == 1){
+								board[xi][yi] = 1;
+                                board[xf][yf] = 1;
+								if(xf+1 == 7 && valorPeca == 3){
+									board[xf+1][yf-1] = -3;
+								}else{
+									board[xf+1][yf-1] = valorPeca;
+								}
+								return board;
+							}
+							return [];
+						}
+						return [];
+					}
+					else{
+						if (yf +1 < 8){
+							if (board[xf+1][yf+1] == 1){
+								board[xi][yi] = 1;
+                                board[xf][yf] = 1;
+								if(xf+1 == 7 && valorPeca == 3){
+									board[xf+1][yf+1] = -3;
+								}else{
+									board[xf+1][yf+1] = valorPeca;
+								}
+								return board;
+							}
+							return [];
+						}
+						return [];
+					}
+				}return [];
+                                       
+			}else if(board[xf][yf] == 1){
+                board[xi][yi] = 1;
+                if(xf == 7 && valorPeca ==3){
+                    board[xf][yf] = -3;
+				}else{
+                    board[xf][yf] = valorPeca;
+                     
+				return board;
+				}
+			}else{
+				return [];
+			}
+				
+			
+		}
+	}
+	
+	/*seleciona peças a serem jogadas*/
 	$('body').on('click','.square',function(){
 		
 		var tileId = $(this).attr('id');
@@ -45,11 +122,13 @@ $(function(){
 			
 		if(selected == 1){
 			$('#'+selectedPiece).fadeTo(10,1);
-			if(board[i][k] == 1){
-				board[i][k] = 2;
+			
+			if(board[i][k] != 0){
+				//board[i][k] = 2;
 			var m = parseInt(selectedPiece.substring(0,1));
 			var n = parseInt(selectedPiece.substring(1));
-			board[m][n] = 1
+			//board[m][n] = 1
+			makeMove(m,n,i,k,3);
 			drawPiece();
 			}
 			selectedPiece = '';
@@ -63,7 +142,7 @@ $(function(){
 			console.log(i);
 			console.log(k);
 			
-			if(board[i][k] == 2){
+			if(board[i][k] == 3){
 				console.log('black');
 				$(this).fadeTo(10,0.5);
 				selected = 1;
